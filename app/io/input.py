a24acd5171041aac91eb4cd4368faba1b5c1eac9
Anton Marynych <antonmarynych6@gmail.com>
2024-03-22 15:33:05 +0100

    added input and output functions with docstrings

---
+++ app/io/input.py
@@ -0,0 +1,25 @@
+def input_text_from_console():
+    """
+    Inputs the text from console
+    """
+    pass
+
+
+def read_from_file_builtin(file_path):
+    """
+    Reads from the file using the built-in options
+
+    Parameters:
+        file_path (str): Path to the file to read from
+    """
+    pass
+
+
+def read_from_file_pandas(file_path):
+    """
+    Reads from the file using pandas
+
+    Parameters:
+        file_path (str): Path to the file to read from
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,19 @@
+def output_text_to_console(text):
+    """
+    Outputs the text to the console
+
+    Parameters:
+        text (str): text that is needed to be written in the console
+    """
+    pass
+
+
+def write_to_file_builtin(file_path, content):
+    """
+    Writes to the file
+
+    Параметри:
+        file_path (str): Path to the file that we want write something to
+        content (str): The content that needs to be written in the file
+    """
+    pass
